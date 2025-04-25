7699281a115a356051d8b97df7bac949ae479819
Cezary Olborski <cezary.olborski@gmail.com>
2025-04-25 20:26:27 +0800

    chore: ChainSpec - bootnodes + telemetry

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,5 +1,6 @@
 use resonance_runtime::WASM_BINARY;
 use sc_service::{ChainType, Properties};
+use sc_telemetry::TelemetryEndpoints;
 use serde_json::json;
 use resonance_runtime::genesis_config_presets::LIVE_TESTNET_RUNTIME_PRESET;
 
@@ -51,6 +52,16 @@ pub fn live_testnet_chain_spec() -> Result<ChainSpec, String> {
 	properties.insert("tokenDecimals".into(), json!(9));
 	properties.insert("tokenSymbol".into(), json!("REZ"));
 
+	let telemetry_endpoints = TelemetryEndpoints::new(vec![
+		("/dns/telemetry.res.fm/tcp/443/x-parity-wss/%2Fsubmit%2F".to_string(), 0)
+	]).expect("Telemetry endpoints config is valid; qed");
+
+	let boot_nodes = vec![
+		"/dns/a1.t.res.fm/tcp/30201/p2p/12D3KooWGmDZ95J13cggsv56mSepAj3WiVPR3foqqh728umZrhPr".parse().unwrap(),
+		"/dns/a2.t.res.fm/tcp/30203/p2p/12D3KooWPPv8nrVEN5mjcMruDnAEdcpfppSfSbij2A7FXWNGt8JL".parse().unwrap(),
+		"/dns/a3.t.res.fm/tcp/30202/p2p/12D3KooWMpmEQmCB31Dz84YdnxL48aiSFQydEiq5MZv6VtZouXRd".parse().unwrap(),
+	];
+
 	Ok(ChainSpec::builder(
 		WASM_BINARY.ok_or_else(|| "Live testnet wasm not available".to_string())?,
 		None,
@@ -58,6 +69,8 @@ pub fn live_testnet_chain_spec() -> Result<ChainSpec, String> {
 	.with_name("Resonance Testnet")
 	.with_id("resonance_testnet")
 	.with_protocol_id("resonance-testnet")
+	.with_boot_nodes(boot_nodes)
+	.with_telemetry_endpoints(telemetry_endpoints)
 	.with_chain_type(ChainType::Live)
 	.with_genesis_config_preset_name(LIVE_TESTNET_RUNTIME_PRESET)
 	.with_properties(properties)
