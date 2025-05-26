9ab7282b6da8fbb7c06c6500cbc8426ebb9988f8
Dastan <88332432+dastansam@users.noreply.github.com>
2025-05-26 13:12:07 +0600

    Make benchmarks compile (#29)

--- pallets/merkle-airdrop/src/benchmarking.rs
+++ pallets/merkle-airdrop/src/benchmarking.rs
@@ -8,7 +8,6 @@ use frame_benchmarking::v2::*;
 use frame_system::RawOrigin;
 extern crate alloc;
 use alloc::vec;
-use frame_support::traits::fungible::{Inspect, Mutate};
 use frame_support::BoundedVec;
 use sp_runtime::traits::Saturating;
 
@@ -20,9 +19,16 @@ mod benchmarks {
     fn create_airdrop() {
         let caller: T::AccountId = whitelisted_caller();
         let merkle_root = [0u8; 32];
+        let vesting_period = None;
+        let vesting_schedule = None;
 
         #[extrinsic_call]
-        create_airdrop(RawOrigin::Signed(caller), merkle_root);
+        create_airdrop(
+            RawOrigin::Signed(caller),
+            merkle_root,
+            vesting_period,
+            vesting_schedule,
+        );
     }
 
     #[benchmark]
@@ -31,19 +37,31 @@ mod benchmarks {
         let merkle_root = [0u8; 32];
 
         let airdrop_id = MerkleAirdrop::<T>::next_airdrop_id();
-        AirdropMerkleRoots::<T>::insert(airdrop_id, merkle_root);
-        AirdropCreators::<T>::insert(airdrop_id, caller.clone());
+        AirdropInfo::<T>::insert(
+            airdrop_id,
+            AirdropMetadata {
+                merkle_root,
+                balance: 0u32.into(),
+                creator: caller.clone(),
+                vesting_period: None,
+                vesting_delay: None,
+            },
+        );
+
         NextAirdropId::<T>::put(airdrop_id + 1);
 
         let amount: BalanceOf<T> = 1u32.into();
 
         // Get ED and ensure caller has sufficient balance
-        let ed = <T::Currency as Inspect<T::AccountId>>::minimum_balance();
+        let ed = <T::Currency as Currency<T::AccountId>>::minimum_balance();
 
         let caller_balance = ed.saturating_mul(10u32.into()).saturating_add(amount);
-        <T::Currency as Mutate<T::AccountId>>::set_balance(&caller, caller_balance);
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(&caller, caller_balance);
 
-        <T::Currency as Mutate<T::AccountId>>::set_balance(&MerkleAirdrop::<T>::account_id(), ed);
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(
+            &MerkleAirdrop::<T>::account_id(),
+            ed,
+        );
 
         #[extrinsic_call]
         fund_airdrop(RawOrigin::Signed(caller), airdrop_id, amount);
@@ -60,20 +78,34 @@ mod benchmarks {
         let merkle_root = leaf_hash;
 
         let airdrop_id = MerkleAirdrop::<T>::next_airdrop_id();
-        AirdropMerkleRoots::<T>::insert(airdrop_id, merkle_root);
+        AirdropInfo::<T>::insert(
+            airdrop_id,
+            AirdropMetadata {
+                merkle_root,
+                balance: amount,
+                creator: caller.clone(),
+                vesting_period: None,
+                vesting_delay: None,
+            },
+        );
+
         NextAirdropId::<T>::put(airdrop_id + 1);
 
-        let ed = <T::Currency as Inspect<T::AccountId>>::minimum_balance();
+        let ed = <T::Currency as Currency<T::AccountId>>::minimum_balance();
         let large_balance = ed.saturating_mul(1_000_000u32.into());
 
-        <T::Currency as Mutate<T::AccountId>>::set_balance(&caller, large_balance);
-        <T::Currency as Mutate<T::AccountId>>::set_balance(&recipient, large_balance);
-        <T::Currency as Mutate<T::AccountId>>::set_balance(
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(&caller, large_balance);
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(&recipient, large_balance);
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(
             &MerkleAirdrop::<T>::account_id(),
             large_balance,
         );
 
-        AirdropBalances::<T>::insert(airdrop_id, large_balance);
+        AirdropInfo::<T>::mutate(airdrop_id, |info| {
+            if let Some(info) = info {
+                info.balance = large_balance;
+            }
+        });
 
         let empty_proof = vec![];
         let merkle_proof = BoundedVec::<MerkleHash, T::MaxProofs>::try_from(empty_proof)
@@ -90,21 +122,35 @@ mod benchmarks {
 
         // Create an airdrop first
         let airdrop_id = MerkleAirdrop::<T>::next_airdrop_id();
-        AirdropMerkleRoots::<T>::insert(airdrop_id, merkle_root);
-        AirdropCreators::<T>::insert(airdrop_id, caller.clone());
+
+        AirdropInfo::<T>::insert(
+            airdrop_id,
+            AirdropMetadata {
+                merkle_root,
+                balance: 0u32.into(),
+                creator: caller.clone(),
+                vesting_period: None,
+                vesting_delay: None,
+            },
+        );
+
         NextAirdropId::<T>::put(airdrop_id + 1);
 
-        let ed = <T::Currency as Inspect<T::AccountId>>::minimum_balance();
+        let ed = <T::Currency as Currency<T::AccountId>>::minimum_balance();
         let tiny_amount: BalanceOf<T> = 1u32.into();
         let large_balance = ed.saturating_mul(1_000_000u32.into());
 
-        <T::Currency as Mutate<T::AccountId>>::set_balance(&caller, large_balance);
-        <T::Currency as Mutate<T::AccountId>>::set_balance(
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(&caller, large_balance);
+        <T::Currency as Currency<T::AccountId>>::make_free_balance_be(
             &MerkleAirdrop::<T>::account_id(),
             large_balance,
         );
 
-        AirdropBalances::<T>::insert(airdrop_id, tiny_amount);
+        AirdropInfo::<T>::mutate(airdrop_id, |info| {
+            if let Some(info) = info {
+                info.balance = tiny_amount;
+            }
+        });
 
         #[extrinsic_call]
         delete_airdrop(RawOrigin::Signed(caller), airdrop_id);
